75e7e5a8ee06a531d592757555ae585211456489
C-ollins <ocollins444@gmail.com>
2016-12-23 17:41:21 +0100

    Added

---
+++ File.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	FILE * fPointer;
+	fPointer = fopen("collins.txt","w");
+	fprintf(fPointer,"I love MySelf\n");
+	fclose(fPointer);
+	return 0;
+}
